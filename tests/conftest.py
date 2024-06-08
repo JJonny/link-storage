f4c1514ec061606c82127297df9626294ff2f1cd
JJonny <qweer.baranovsky@gmail.com>
2024-06-08 19:06:42 +0200

    Create JWT fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,18 @@
 import asyncio
-from pathlib import Path
-from typing import AsyncGenerator
-
 import pytest
+
+from datetime import datetime, timezone, timedelta
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
+from pathlib import Path
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from typing import AsyncGenerator
 
+from core.config import config
 from database.base_model import Model
 from database.data_input_orm import DataInputOrm
-
 from main import app
-
+from utils.user_constants import ACCESS_TOKEN_TTL, USER_ROLES, PERMISSIONS
 
 TEST_BASE_DIR = Path(__file__).parent.absolute()
 
@@ -44,3 +45,17 @@ app = TestClient(app)
 async def ac() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url='http://test') as ac:
         yield ac
+
+
+@pytest.fixture(scope='session')
+def create_token() -> bytes:
+    payload = {}
+    now = datetime.now(timezone.utc)
+    expire = datetime.timestamp(now + timedelta(hours=ACCESS_TOKEN_TTL))
+    payload["type"] = "access"
+    payload["exp"] = expire
+    payload["iat"] = datetime.utcnow()
+    payload["sub"] = "123-456"
+    payload["perms"] = [{USER_ROLES.admin: PERMISSIONS.admin_permissions}]
+
+    yield jwt.encode(payload, config.JWT_SECRET)
